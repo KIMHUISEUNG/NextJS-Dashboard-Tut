bf363f1e539074648403cb0b2ede70a3869e284c
hatso <mmorpg3636@naver.com>
2025-03-13 18:11:47 +0900

    delete openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,20 +9,6 @@ export const metadata: Metadata = {
   },
   description: '공식 Next.js Learn 과정에서 배포한 App Router.',
   icons: "/favicon.ico",
-  openGraph: {
-    title: "HatsoDB App Router",
-    description: "공식 Next.js Learn 과정에서 배포한 App Router.",
-    siteName: "HatsoDB project",
-    images: [
-      {
-        url: "/favicon.ico", // OG 이미지 (공유 썸네일)
-        width: 1200,
-        height: 630,
-        alt: "내 프로젝트 썸네일 이미지",
-      },
-    ],
-    type: "website",
-  },
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
