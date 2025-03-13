369f34bfc6b4f3885990e2ff433ea59f85955e1d
hatso <mmorpg3636@naver.com>
2025-03-13 17:37:33 +0900

    update openGraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,26 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | Hatso Dashboard',
+    default: 'Hatso Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
+  description: '공식 Next.js Learn 과정에서 배포한 App Router.',
   icons: "/favicon.ico",
+  openGraph: {
+    title: "HatsoDB | HatsoDB App Router",
+    description: "공식 Next.js Learn 과정에서 배포한 App Router.",
+    url: "https://next-js-dashboard-tut-nine.vercel.app/",
+    siteName: "HatsoDB project",
+    images: [
+      {
+        url: "/favicon.ico", // OG 이미지 (공유 썸네일)
+        width: 1200,
+        height: 630,
+        alt: "내 프로젝트 썸네일 이미지",
+      },
+    ],
+    type: "website",
+  },
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
